52cbbdda845ad90f97bcda20314b10d9ec8ae5b7
zhixun <327354573@qq.com>
2021-04-09 18:02:56 +0800

    20210409

--- 结构体.cpp
+++ 结构体.cpp
@@ -2,21 +2,27 @@
 using namespace std;
 struct Student
 {
-    int age = 10;
+public:
+    int age = 10; //结构体中定义的成员变量或成员函数默认都是public属性的。
     string name;
     int score;
+    void play();
 } ss; //定义结构体的时候顺便创建变量ss
-
+void Student::play()
+{
+    cout << "定义结构体函数" << endl;
+}
 int main()
 {
     //1.声明结构体变量:前面最好加上struct，也可以省略
-    struct Student s;
+    Student s;
     struct Student s1 = {30, "张三", 100};
 
     Student *s2 = &s1;
     cout << "s.age=" << s.age << "\ts1.name=" << s1.name << endl;
     cout << "s1.age=" << s1.age << endl;
     cout << "s2->age=" << s2->age << endl;
+    ss.play();
     //2.结构体数组
     Student array[2] = {{30, "张三", 100}, {29, "李四", 80}};
     return 0;
