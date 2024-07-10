9a3d565587e8787d4dd4397361db3c01a8f10952
zhixun <327354573@qq.com>
2024-07-10 21:16:20 +0800

    线程分离

--- Linux/process/thread.cpp
+++ Linux/process/thread.cpp
@@ -32,13 +32,24 @@
  *   在线程函数中return NULL 退出线程，可以将NULL或nullptr等效void*类型的空指针
  * 5.等待线程终止
  *   pthread_join() //void ** ,就传进去一个 void *joinValue;  &joinValue 类型取得时候再转成int类型
- *   成功返回0
+ *   成功返回0 失败返回错误号
  * 6.取消线程
- *   pthread_cancel()
+ *   pthread_cancel(t_id) 成功返回0，被pthread_cancel 终止的线程必须要有取消点，否则无法取消tfun5 的取消点就是sleep。还有很多其他的取消点，黑马老师没说
+ *   pthread_testcancel 没有任何逻辑的取消点，建议用这个。跟协程的不同，只要线程函数执行过pthread_testcancel，pthread_cancel就能立马取消线程
+ *   终止线程的三种方法， 线程函数中 return 、pthread_exit()\、 线程外 执行pthread_cancel()
+ *   终止线程之后再执行join 会得到什么呢？？？黑马老师说事-1 千寻的回答打印的是 PTHREAD_CANCELED， 我的报错啥都没打印出来
+ * 7.线程分离
+ *   pthread_detach(tid) 成功返回0，失败返回错误号。 执行pthread_detach的线程，不能再执行pthread_join
+ *   当一个线程处于分离状态时，它的资源在其终止时会被自动释放，而不需要其他线程调用pthread_join来进行资源回收。这意味着线程可以在后台运行，不需要其他线程等待它的结束并回收资源。
+ *   pthread_join 回收？？？ 线程中的方法不是在栈中执行吗？执行完了会自动回收的呀，为什么还要他线程调用pthread_join来进行资源回收
+ *   
+ *
+ *
  *
  * 子线程被创建出来之后需要抢cpu时间片, 抢不到就不能运行，如果主线程退出了, 虚拟地址空间就被释放了, 子线程就一并被销毁了。但是如果某一个子线程退出了, 主线程仍在运行, 虚拟地址空间依旧存在。
  * 编译的时候要加上 -lpthread 参数
  * reinterpret_cast 类型转换.例如 int 转 void* reinterpret_cast<void *>(传入一个int变量)
+ * 以上线程相关方法 如果错误后执行perror 不能得到正确的翻译，这是因为 线程相关方法直接返回错误码，没有写入error_num,所以要用strerror(errnum)翻译错误码
  */
 void *tfun(void *arge)
 {
@@ -80,6 +91,16 @@ void *tfun4(void *arge)
     *ret_value = 30;
     pthread_exit(ret_value);
 }
+void *tfun5(void *arge)
+{
+    // pthread_testcancel();
+    while (1)
+    {
+        printf("线程创建成功 id = %ld ，等待取消\n", pthread_self(), getpid());
+        sleep(1);
+    }
+    pthread_exit(NULL);
+}
 int main()
 {
 
@@ -118,15 +139,30 @@ int main()
     // 5. 等待线程终止
     printf("5. 等待线程终止-----------------------------\n");
     pthread_create(&tid, NULL, tfun4, NULL);
-    void *joinValue;//没看明白类型转换
+    void *joinValue;                     // 没看明白类型转换
     ret = pthread_join(tid, &joinValue); // void ** ,就传进去一个 void *joinValue 类型取得时候再转成int类型
     if (ret != 0)
     { // 成功返回0
         perror("pthread_join 失败");
     }
-    printf("等待线程终止var=%d\n", *((int *)joinValue));
-    void * value  = new int(40); 
-    printf("value=%d\n", *((int *)value));
+    printf("等待线程终止var=%d\n", *(static_cast<int *>(joinValue)));
+    void *value = malloc(sizeof(void *));
+
+    // 5. 取消线程
+    printf("6. 取消线程-----------------------------\n");
+    pthread_create(&tid, NULL, tfun5, NULL);
+    sleep(3);
+    ret = pthread_cancel(tid);
+    pthread_join(tid, &value);
+    if (ret == 0)
+    {
+        printf("线程取消成功 id = %ld\n", tid);
+        // printf("线程取消后再join会得到什么？  %d\n", *(static_cast<int*>(value)));
+    }
+    else
+    {
+        perror("线程取消失败---");
+    }
     pthread_exit((void *)0); // 退出主线程，不影响子线程，这样就不必sleep等待子线程执行完再结束进程
     return 1;
 }
