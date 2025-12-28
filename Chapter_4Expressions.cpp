97dec403c32b45d91d090f83d83828324f214b57
zhixun <327354573@qq.com>
2025-12-28 23:49:01 +0800

    表达式

---
+++ Chapter_4Expressions.cpp
@@ -0,0 +1,21 @@
+// /d:/CppCode/Chapter_4Expressions.cpp
+// 简单的 C++ 程序，展示基本表达式与输出
+
+#include <iostream>
+/**
+ * 第四章 表达式
+ * 4.1.1 基本概念
+ *       一元运算符、二元运算符、三元运算符
+ *       作用与一个运算对象的运算符是一元运算符，例如 -a 表示取负 &a 表示取地址 *表示解引用
+ *       作用于两个运算对象的是二元运算符，例如 a + b 、a == b 
+ *       函数调用也是一种特殊的运算符，对运算对象数量没有限制
+ *       运算符两侧的对象通常会转换成统一类型.例如 bool char short 转换成 int 、 float转成double
+ *       重载运算符作用于类对象
+ *       C++的表达式分为右值（rvalue）和左值（lvalue）。当一个对象被用作右值的时候，用的是对象的值（内容）；当对象被用作左值时，用的是对象的地址。需要右值的地方可以用左值代替，反之则不行
+ * sizeof运算符
+ * 四种显式转换 cast-name是static_cast、dynamic_cast、const_cast和reinterpret_cast中的一种，用来指定转换的方式。
+ */
+int main() {
+    
+    return 0;
+}
